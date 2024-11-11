7a0fca24b3bba09af7104d966302db552c35cba6
Talha Bey <muhammadfarhan3789076@gmail.com>
2024-11-10 19:40:44 -0800

    fixing

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -31,8 +31,7 @@ export async function POST(request) {
     const cookieStore = cookies();
     cookieStore.set("authtoken", authToken, {
       sameSite: "none",
-      domain: ".vercel.app",
-
+      
       secure: true,
       path: "/",
       maxAge: 30 * 24 * 60 * 60,
